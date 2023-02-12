21ee0616733f1b23056917bdcfa934f9e323942c
Spike Grobstein <me@spike.cx>
2023-02-11 21:44:29 -0800

    add support for getting and removing and modifying users in the registry

--- src/server/user_registry.rs
+++ src/server/user_registry.rs
@@ -10,6 +10,18 @@ impl UserRegistry {
         self.counter += 1;
         self.counter
     }
+
+    pub fn remove_user(&mut self, id: u32) {
+        self.users.retain(|u| u.id != id);
+    }
+
+    pub fn get_user(&self, id: u32) -> Option<&User> {
+        self.users.iter().find(|u| u.id == id)
+    }
+
+    pub fn get_user_mut(&mut self, id: u32) -> Option<&mut User> {
+        self.users.iter_mut().find(|u| u.id == id)
+    }
 }
 
 
